752de876e476813a337bbe412333b9b2cadafbfd
jiyoon-00 <pulip2517@gmail.com>
2025-08-20 01:51:41 +0900

    [feat] 생성자 기반 DI 구현 및 의존성 해석 추가

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -6,9 +6,8 @@ import com.ll.framework.ioc.annotations.Service;
 import org.reflections.Reflections;
 
 import java.beans.Introspector;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.lang.reflect.Constructor;
+import java.util.*;
 
 public class ApplicationContext {
     private final String basePackage;
@@ -21,13 +20,15 @@ public class ApplicationContext {
 
     public void init() {
         Reflections r = new Reflections(basePackage);
-        Set<Class<?>> comps = r.getTypesAnnotatedWith(Component.class);
-        Set<Class<?>> svcs = r.getTypesAnnotatedWith(Service.class);
-        Set<Class<?>> repos = r.getTypesAnnotatedWith(Repository.class);
+        Set<Class<?>> all = new HashSet<>();
+        all.addAll(r.getTypesAnnotatedWith(Component.class));
+        all.addAll(r.getTypesAnnotatedWith(Service.class));
+        all.addAll(r.getTypesAnnotatedWith(Repository.class));
 
-        for (Class<?> c : comps) catalog.put(Introspector.decapitalize(c.getSimpleName()), c);
-        for (Class<?> c : svcs) catalog.put(Introspector.decapitalize(c.getSimpleName()), c);
-        for (Class<?> c : repos) catalog.put(Introspector.decapitalize(c.getSimpleName()), c);
+        for (Class<?> c : all) {
+            String name = Introspector.decapitalize(c.getSimpleName());
+            catalog.put(name, c);
+        }
     }
 
     @SuppressWarnings("unchecked")
@@ -38,8 +39,44 @@ public class ApplicationContext {
         if (cached != null) return (T) cached;
 
         Class<?> type = catalog.get(key);
-        if (type == null) throw new RuntimeException("빈을 찾을 수 없음: " + key);
+        if (type == null) throw new NoSuchElementException("빈을 찾을 수 없음: " + key);
 
-        return null;
+        Constructor<?> ctor = pickCtor(type);
+        try {
+            Class<?>[] pts = ctor.getParameterTypes();
+            Object[] args = new Object[pts.length];
+
+            for (int i = 0; i < pts.length; i++) {
+                args[i] = genBean(resolveName(pts[i]));
+            }
+
+            ctor.setAccessible(true);
+            Object instance = (pts.length == 0) ? ctor.newInstance() : ctor.newInstance(args);
+            singletons.put(key, instance);
+            return (T) instance;
+        } catch (ReflectiveOperationException e) {
+            throw new IllegalStateException("객체 생성 실패: " + type.getName(), e);
+        }
+    }
+
+    private Constructor<?> pickCtor(Class<?> c) {
+        Constructor<?>[] ctors = c.getDeclaredConstructors();
+        Constructor<?> best = ctors[0];
+        for (Constructor<?> x : ctors) {
+            if (x.getParameterCount() > best.getParameterCount()) best = x;
+        }
+        return best;
+    }
+
+    private String resolveName(Class<?> paramType) {
+        String direct = Introspector.decapitalize(paramType.getSimpleName());
+        if (catalog.containsKey(direct)) return direct;
+
+        for (Map.Entry<String, Class<?>> e : catalog.entrySet()) {
+            if (paramType.isAssignableFrom(e.getValue())) {
+                return e.getKey();
+            }
+        }
+        throw new NoSuchElementException("적절한 구현체를 찾을 수 없음: " + paramType.getName());
     }
 }
